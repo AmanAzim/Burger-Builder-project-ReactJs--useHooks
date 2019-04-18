029bb6a856003d2b9b8d87e5ada142f76130a878
Aman <rumman.djuice1@gmail.com>
2019-04-18 19:07:28 +0200

    "Commit"

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, {useEffect} from 'react'
 import Order from '../../components/Order/Order'
 import axios from '../../axios-orders'
 import ErrorHandler from '../../hoc/ErrorHandler/ErrorHandler'
@@ -6,26 +6,25 @@ import * as Actions from "../../store/actions/exportAllActions";
 import {connect} from 'react-redux'
 import Spinner from '../../components/UI/Spinner/Spinner'
 
-class Orders extends Component{
+const Orders =(props)=>{
 
-    componentDidMount() {
-        this.props.onFetchOrders(this.props.token, this.props.userId);
+    useEffect(()=>{
+        props.onFetchOrders(props.token, props.userId);
+    }, []);
+
+    let orders=<Spinner/>;
+
+    if(!props.loading){
+        orders=props.orders.map( order=>{
+            return <Order key={order.id} ingredients={order.ingredients} price={+order.price}/>
+        })
     }
 
-    render(){
-        let orders=<Spinner/>;
-
-        if(!this.props.loading){
-            orders=this.props.orders.map( order=>{
-                      return <Order key={order.id} ingredients={order.ingredients} price={+order.price}/>
-                   })
-        }
-        return(
-            <div>
-                {orders}
-            </div>
-        )
-    }
+    return(
+        <div>
+            {orders}
+        </div>
+    )
 }
 
 const mapStateToProps=state=>{
